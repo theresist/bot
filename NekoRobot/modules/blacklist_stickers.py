568378018c28334e53d6212610e13fd69c6d8db0
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-06-12 03:24:02 +0530

    Update blacklist_stickers.py

--- NekoRobot/modules/blacklist_stickers.py
+++ NekoRobot/modules/blacklist_stickers.py
@@ -470,7 +470,7 @@ Note:
  • `<sticker link>` can be `https://t.me/addstickers/<sticker>` or just `<sticker>` or reply to the sticker message
 """
 
-__mod_name__ = "Stickers Blacklist"
+__mod_name__ = "𝑆𝑡𝑖𝑐𝑘𝑒𝑟𝑠~𝐵𝑙𝑎𝑐𝑘𝑙𝑖𝑠𝑡"
 
 BLACKLIST_STICKER_HANDLER = DisableAbleCommandHandler(
     "blsticker", blackliststicker, admin_ok=True, run_async=True
