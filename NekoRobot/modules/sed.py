95ca9dfcbcf56239fdee6cd66ad9bfa6a9d339a2
|•| Tʜᴇ Sʜɪᴠ Oғғɪᴄɪᴀɪʟ ⋟ <126413724+WASTERTSO@users.noreply.github.com>
2023-06-12 03:33:02 +0530

    Update sed.py

--- NekoRobot/modules/sed.py
+++ NekoRobot/modules/sed.py
@@ -160,7 +160,7 @@ If you want to use these characters, make sure you escape them!
     telegram.MAX_MESSAGE_LENGTH
 )
 
-__mod_name__ = "Sed/Regex"
+__mod_name__ = "𝑆𝑒𝑑~𝑅𝑒𝑔𝑒𝑥"
 
 SED_HANDLER = DisableAbleMessageHandler(
     Filters.regex(r"s([{}]).*?\1.*".format("".join(DELIMITERS))),
