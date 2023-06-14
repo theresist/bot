d0592af5a190b1a98246baf7c3902feef24e6adb
Dipesh <83916289+Dipxu@users.noreply.github.com>
2023-06-14 10:48:25 +0530

    Update __main__.py

--- NekoRobot/__main__.py
+++ NekoRobot/__main__.py
@@ -229,6 +229,16 @@ for module_name in ALL_MODULES:
 
 # do not async
 
+def send_help(chat_id, text, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    NEKO_PTB.bot.send_message(
+        chat_id=chat_id,
+        text=text,
+        parse_mode=ParseMode.MARKDOWN,
+        disable_web_page_preview=True,
+        reply_markup=keyboard,
+    )
 
 
 
